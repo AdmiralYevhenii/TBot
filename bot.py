6e329844db534f95179dd8ebb3912ec42c4c0e22
Yevhenii Kostohryz <ykostoh@intercars.eu>
2024-12-13 13:18:45 +0200

    Виправлення 0.04v

--- bot.py
+++ bot.py
@@ -1,20 +1,18 @@
 from flask import Flask, request
 from telegram import Bot
-from telegram.utils.request import Request
+from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 import asyncio
 
-# Токен бота 
-TOKEN = "8029573466:AAFq4B_d-s73bPG0z9kRcOAU2sE3wFwAsj4"  # токен бота
-WEBHOOK_URL = "https://tbot-pexl.onrender.com"  # URL на Render
+# Токен вашого бота
+TOKEN = "8029573466:AAFq4B_d-s73bPG0z9kRcOAU2sE3wFwAsj4"  # Ваш токен бота
+WEBHOOK_URL = "https://tbot-pexl.onrender.com"  # Ваш URL на Render
 
-# Налаштування пулу з'єднань
-request = Request(con_pool_size=8)  # Збільшуємо розмір пулу
-bot = Bot(token=TOKEN, request=request)
 app = Flask(__name__)
 
 async def send_message(chat_id, text):
     """Асинхронна функція для відправки повідомлення."""
+    bot = Bot(token=TOKEN)
     await bot.send_message(chat_id=chat_id, text=text)
 
 @app.route(f"/{TOKEN}", methods=["POST"])
@@ -35,5 +33,8 @@ def webhook():
 
 if __name__ == "__main__":
     # Встановлення вебхука
+    bot = Bot(token=TOKEN)
     bot.set_webhook(url=f"{WEBHOOK_URL}/{TOKEN}")
+
+    # Запускаємо Flask сервер
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
